13448f2b19c0be6413d18e31b4dad62915973aa1
Phlevian <efedhaphlevian@gmail.com>
2025-09-14 20:23:06 +0300

    Add 10-factorial.js: recursive factorial function

---
+++ 10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+function factorial(n) {
+  if (n <= 1) return 1;
+  return n * factorial(n - 1);
+}
+
+const arg = process.argv[2];
+const num = parseInt(arg, 10);
+
+if (isNaN(num)) {
+  console.log(1);
+} else {
+  console.log(factorial(num));
+}
